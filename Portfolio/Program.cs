c5d15bce20cc208f94779fa1dd53b3f510071a05
Mircea Halalai <boss.mircea67@yahoo.com>
2025-09-25 20:55:05 +0300

    Init PhotoViewer

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portfolio;
+using Portfolio.Common;
 using Portfolio.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddSingleton(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddSingleton<IconRegistry>();
+builder.Services.AddSingleton<PhotoViewer.Service>();
 
 var host = builder.Build();
 
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A nice and efficient solution for reusing and preloading SVGs is with &lt;use>
 
 `cd wwwroot/`
 
-`npx svg-sprite --symbol --dest=./ raw-svg/*.svg --log=verbose`
+`npx svg-sprite --symbol --dest=./ raw-svg/*.svg --log=verbose --shape-id-generator i-%s`
 
 NOTE: use verbose to check for errors. Such as BOM encoded svg files. Using Notepad++ to strip the BOM is a solution
