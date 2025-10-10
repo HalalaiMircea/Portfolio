b9be6e56f0fa54dacd5167f8ef29f6f5f5696420
Mircea Halalai <boss.mircea67@yahoo.com>
2025-10-10 03:12:51 +0300

    Add Third Party License Notices for legal compliance

--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,3 +1,4 @@
+using Markdig;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Portfolio;
@@ -11,6 +12,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddSingleton<IconRegistry>();
 builder.Services.AddSingleton<PhotoViewer.Service>();
+builder.Services.AddSingleton(new MarkdownPipelineBuilder()
+    .UsePipeTables()
+    .UseAutoLinks()
+    .Build()
+);
 
 var host = builder.Build();
 
